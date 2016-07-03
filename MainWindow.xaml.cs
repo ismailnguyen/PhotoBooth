a92583c0af35ed86b95f487ff27a545092d691c6
ismailnguyen <me@nguyenismail.com>
2016-07-03 02:14:09 +0200

    Auto start camera.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Windows;
-using System.Windows.Controls;
 using WebEye.Controls.Wpf;
 using PhotoBooth.Services;
 using System.Drawing;
 using System.Collections.Generic;
 using System.Linq;
-using MahApps.Metro.Controls.Dialogs;
 
 namespace PhotoBooth
 {
@@ -21,52 +19,38 @@ namespace PhotoBooth
         {
             InitializeComponent();
 
-            InitializeComboBox();
-
-            //startCamera();
-
             _cloudService = new CloudService();
 
-            
-        }
-
-        private void InitializeComboBox()
-        {
-            comboBox.ItemsSource = webCameraControl.GetVideoCaptureDevices();
-
-            if (comboBox.Items.Count > 0)
-            {
-                comboBox.SelectedItem = comboBox.Items[0];
-            }            
-        }
-
-        private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            startButton.IsEnabled = e.AddedItems.Count > 0;
+            Loaded += new RoutedEventHandler(MainWindowLoaded);   
         }
 
         /// <summary>
-        /// On button start click, hide button, start camera and show it
+        /// Auto start camera at application launch
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void OnStartButtonClick(object sender, RoutedEventArgs e)
+        private void MainWindowLoaded(object sender, RoutedEventArgs e)
         {
-            //MessageDialogResult result = this.ShowMessageAsync("Event name", "Some message").ConfigureAwait(false);
-
-            startCamera();
+            if (IsLoaded)
+            {
+                startCamera();
+            }
         }
 
         private void startCamera()
         {
-            var cameraId = (WebCameraId)comboBox.SelectedItem;
-            webCameraControl.StartCapture(cameraId);
+            List<WebCameraId> cameras = new List<WebCameraId>(webCameraControl.GetVideoCaptureDevices());
+            webCameraControl.StartCapture(cameras.FirstOrDefault());
 
-            startButton.Visibility = Visibility.Hidden;
             webCameraControl.Visibility = Visibility.Visible;
             takePhotoButton.Visibility = Visibility.Visible;
         }
 
+        /// <summary>
+        /// Show "About" panel on right side
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void OnAboutClick(object sender, RoutedEventArgs e)
         {
             flyout.IsOpen = !flyout.IsOpen;
@@ -76,25 +60,25 @@ namespace PhotoBooth
         {
             if (webCameraControl.IsCapturing)
             {
-                /*var dialog = new SaveFileDialog { Filter = "Bitmap Image|*.bmp" };
-                if (dialog.ShowDialog() == true)
-                {
-                    webCameraControl.GetCurrentImage().Save(dialog.FileName);
-                }*/
-
+                // Generate photo name with current date
                 string photoName = string.Format(
                             "{0}-{1}.jpg",
-                            "Mariage",
+                            "PhotoBooth",
                             DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss")
                         );
 
-
+                // Apply filter on photo
                 IProcessing process = new Processing(webCameraControl.GetCurrentImage());
                 Bitmap result = process.negativ();
 
-                result.Save(photoName);
+                // Save photo to local storage
+                //result.Save(photoName);
 
-                _cloudService.Upload(photoName);
+                FiltersWindow filtersWindow = new FiltersWindow();
+                filtersWindow.Show();
+
+                // Save photo to cloud storage
+                //_cloudService.Upload(photoName);
             }
         }
     }
